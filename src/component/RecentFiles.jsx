b149e888d11310a8e843e56179c9c118f9d05ed2
Akaywonder1124 <akaycopy@gmail.com>
2023-11-07 10:02:15 +0100

    commit

--- src/component/RecentFiles.jsx
+++ src/component/RecentFiles.jsx
@@ -78,7 +78,7 @@ function RecentFiles() {
 }
 function Loading() {
   return (
-    <div className="text-3xl max-md:text-md text-[#e0edff]">
+    <div className="text-3xl max-md:text-md text-[#0048AD] text-center font-semibold">
       Fetching data ....
     </div>
   );
